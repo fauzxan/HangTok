b1f8a65851e499d1a58774cddce48dd2e829e591
Ferry Chandra <56271017+FerryC13@users.noreply.github.com>
2022-05-28 08:26:37 +0800

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Hangman</h1>
+        <p>Find the hidden word - Enter a letter</p>
+      </>
+    );
+  }
+}
